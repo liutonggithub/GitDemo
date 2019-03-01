d916910f4b2daf074b1157b80cfa4070ca31e9ad
Liu Tong <1512403032@qq.com>
2019-03-01 21:54:35 +0800

    这是版本1.

---
+++ git_prictice.py
@@ -0,0 +1,14 @@
+"""这是github的demo"""
+my_list = ["jake", "Tom", "xi li"]
+
+# for name in my_list:
+#     print(name)
+
+# for number in range(11):
+#     print(number)
+
+
+for letter in 'inty':
+    print("each letter is: " + letter)
+
+'''这是github的demo'''
